e08da1ed678e8e1ef58d059f523d9a617dc81954
periscuelo <periscuelo@gmail.com>
2024-08-23 17:57:56 -0300

    Added swagger comments to show the correct params on docs

--- src/routes/Comment.routes.ts
+++ src/routes/Comment.routes.ts
@@ -4,10 +4,39 @@ import CommentController from '../controller/Comment.controller';
 const router = Router();
 
 router.patch('/comments/move', (req: Request, res: Response) => {
+  /*
+    #swagger.tags = ['Comments']
+    #swagger.parameters['body'] = {
+      in: 'body',
+      required: 'true',
+      schema: {
+          $from: 4,
+          $to: 5
+      }
+    }
+  */
   CommentController.moveComments(req, res);
 });
 
 router.post('/comments/add/:postId', (req: Request, res: Response) => {
+  /*
+    #swagger.tags = ['Comments']
+    #swagger.parameters['postId'] = {
+      in: 'path',
+      required: 'true',
+      type: 'number',
+      description: 'Post Id to add comments'
+    }
+    #swagger.parameters['body'] = {
+      in: 'body',
+      required: 'true',
+      schema: {
+          $body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis dui scelerisque, facilisis nunc sit amet, dictum lectus. Proin pulvinar.",
+          $name: "John Doe",
+          $email: "john.doe@gmail.com"
+      }
+    }
+  */
   CommentController.createComment(req, res);
 });
 
--- src/routes/Post.routes.ts
+++ src/routes/Post.routes.ts
@@ -10,19 +10,69 @@ const cache = expeditiousCache({
 const router = Router();
 
 router.get('/posts', cache.withTtlForStatus('1 minute', 404), (req: Request, res: Response) => {
+  /*
+    #swagger.tags = ['Posts']
+    #swagger.parameters['page'] = {
+      in: 'query',
+      type: 'number'
+    }
+    #swagger.parameters['limit'] = {
+      in: 'query',
+      type: 'number'
+    }
+  */
   PostController.getPosts(req, res);
 });
 
 router.get('/posts/:id/comments', cache.withTtlForStatus('1 minute', 404), (req: Request, res: Response) => {
+  /*
+    #swagger.tags = ['Posts']
+    #swagger.parameters['id'] = {
+      in: 'path',
+      required: 'true',
+      type: 'number',
+      description: 'Post Id to load comments'
+    }
+    #swagger.parameters['page'] = {
+      in: 'query',
+      type: 'number'
+    }
+    #swagger.parameters['limit'] = {
+      in: 'query',
+      type: 'number'
+    }
+  */
   PostController.getComments(req, res);
 });
 
-router.delete('/posts/:id', (req: Request, res: Response) => {
-  PostController.deletePost(req, res);
+router.delete('/posts', (req: Request, res: Response) => {
+  /*
+    #swagger.tags = ['Posts']
+    #swagger.auto = false
+    #swagger.parameters['body'] = {
+      in: 'body',
+      required: 'true',
+      description: 'Post Ids to delete, comments will be cascading!',
+      schema: {
+        ids: [2, 3]
+      }
+    }
+  */
+  PostController.deletePosts(req, res);
 });
 
-router.delete('/posts', (req: Request, res: Response) => {
-  PostController.deletePosts(req, res);
+router.delete('/posts/:id', (req: Request, res: Response) => {
+  /*
+    #swagger.tags = ['Posts']
+    #swagger.auto = false
+    #swagger.parameters['id'] = {
+      in: 'path',
+      required: 'true',
+      type: 'number',
+      description: 'Post Id to delete, comments will be cascading!'
+    }
+  */
+  PostController.deletePost(req, res);
 });
 
 export default router;
